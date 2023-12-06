f2e27ee01b6720a1a2a4a089c18b0f89373d2345
Frankie Fisher <frankie@terrorise.me.uk>
2023-12-06 21:55:20 +0000

    feat: get_leaderboard script
    downloads private leaderboard file

---
+++ get_leaderboard.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# this is a script downloads puzzle input for a particular day 
+# Pass in the day as the first argument to the script
+#
+# to set this up, you need to have filled a file called session_cookie.txt with the cookie
+# named "session" which can be obtained from the browser developer console after you
+# have logged into advent of code.
+# The format is in netscape cookie format (for an example, do wget --save-cookies=example.txt https://google.com)
+#
+
+YEAR=${YEAR:-$(date +%Y)}
+
+BASE_PATH="$(dirname $(realpath $0))"
+
+LEADERBOARD="$(cat leaderboard_id.txt)"
+SAVE_FILE="leaderboard.json"
+
+URL="https://adventofcode.com/$YEAR/leaderboard/private/view/$LEADERBOARD.json"
+
+curl -b "$BASE_PATH"/session_cookie.txt "$URL" -o "$SAVE_FILE"
+
