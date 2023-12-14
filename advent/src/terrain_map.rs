374c9c4cad211cb386a92b156ed5a4bb46f29633
Frankie Fisher <frankie@terrorise.me.uk>
2023-12-14 20:11:47 +0000

    feat: terrain map rotation / accessors
    Point accessors were getting annoying so can now use coordinates to
    retrieve values.
    
    Methods to rotate matrix 90 degrees.

--- advent/src/terrain_map.rs
+++ advent/src/terrain_map.rs
@@ -1,34 +1,84 @@
 use crate::{Point,Point3,Dims,Dims3};
 
 #[derive(Debug)]
+enum Origin {
+  TopLeft,
+  TopRight,
+  BottomRight,
+  BottomLeft,
+}
+
+#[derive(Debug)]
 pub struct TerrainMap<T:Copy+Default> {
-  v: Vec< Vec<T>>, //each inner vector is a horizontal row
+  v: Vec<T>,
+  origin: Origin,
   pub dims: Dims,
 }
 
 impl<T:Copy+Default> TerrainMap<T> {
   pub fn new(dims: Dims) -> TerrainMap<T> {
-    let mut tm:TerrainMap<T> = TerrainMap{ v: Vec::new(), dims };
-    for _ in 0..dims.height {
-      tm.v.push(vec![T::default();dims.width]);
-    }
+    let mut tm:TerrainMap<T> = TerrainMap{ v: Vec::new(), origin: Origin::TopLeft, dims };
+    tm.v.resize_with(dims.width * dims.height, T::default);
     tm
   }
 
   pub fn get(&self, p: &Point) -> T {
-    assert!(p.x >= self.dims.minx);
-    assert!(p.y >= self.dims.miny);
-    assert!(p.x < self.dims.minx.saturating_add_unsigned(self.dims.width));
-    assert!(p.y < self.dims.miny.saturating_add_unsigned(self.dims.height));
-
-    let v = self.v.get(p.y as usize).unwrap();
-    *v.get(p.x as usize).unwrap()
+    self.getc(p.x, p.y)
+  }
+  pub fn getc(&self, x: isize, y: isize) -> T {
+    *self.v.get(self.coords_to_offset(x,y)).unwrap()
   }
   pub fn set(&mut self, p: &Point, val:T) {
-    assert!(p.x >= 0);
-    assert!(p.y >= 0);
-    let v: &mut Vec<T> = self.v.get_mut(p.y as usize).unwrap();
-    v[p.x as usize] = val;
+    self.setc(p.x, p.y, val )
+  }
+
+  pub fn setc(&mut self, x: isize, y: isize, val: T) {
+    let offset = self.coords_to_offset(x, y);
+    self.v[offset] = val;
+  }
+
+  fn coords_to_offset(&self, x: isize, y: isize ) -> usize {
+    assert!(x >= self.dims.minx);
+    assert!(y >= self.dims.miny);
+    assert!(x < self.dims.minx.saturating_add_unsigned(self.dims.width));
+    assert!(y < self.dims.miny.saturating_add_unsigned(self.dims.height));
+
+    let x1:usize = (x-self.dims.minx) as usize;
+    let y1:usize = (y-self.dims.miny) as usize;
+    let (x2,y2) = match self.origin {
+      Origin::TopLeft => (x1, y1),
+      Origin::TopRight => (y1,self.dims.height - x1 -1),
+      Origin::BottomRight => (self.dims.width - x1 -1, self.dims.height - y1 -1),
+      Origin::BottomLeft => (self.dims.width - y1 -1, x1),
+    };
+
+    //println!("x: {x} => {x2}, y: {y} => {y2}");
+    y2 * self.dims.width + x2
+  }
+
+
+  // rotates the matrix 90 degrees clockwise
+  // this is done in place so it's fast (accessors feel the pain)
+  pub fn rotate_cw(&mut self) {
+    assert_eq!(self.dims.width, self.dims.height);
+    self.origin = match self.origin {
+      Origin::TopLeft => Origin::TopRight,
+      Origin::TopRight => Origin::BottomRight,
+      Origin::BottomRight => Origin::BottomLeft,
+      Origin::BottomLeft => Origin::TopLeft,
+    }
+  }
+
+  // rotates the matrix 90 degrees anti-clockwise
+  // this is done in place so it's fast (accessors feel the pain)
+  pub fn rotate_acw(&mut self) {
+    assert_eq!(self.dims.width, self.dims.height);
+    self.origin = match self.origin {
+      Origin::TopLeft => Origin::BottomLeft,
+      Origin::BottomLeft => Origin::BottomRight,
+      Origin::BottomRight => Origin::TopRight,
+      Origin::TopRight => Origin::TopLeft,
+    }
   }
 }
 
@@ -90,6 +140,66 @@ mod tests {
 
       tm.set(&Point3{x:1,y:2,z:3}, 15);
       assert!(tm.get(&Point3{x:1, y:2, z:3}) == 15);
+    }
+
+    #[test]
+    fn test_get_set() {
+      let mut tm = TerrainMap::<usize>::new(Dims{width:5,height:5,..Default::default()});
+      tm.set(&Point{x:1,y:2}, 15);
+      assert!(tm.get(&Point{x:1, y:2}) == 15);
+      tm.set(&Point{x:4,y:4}, 16);
+      assert!(tm.get(&Point{x:4, y:4}) == 16);
+      tm.rotate_cw();
+      tm.set(&Point{x:2,y:3}, 17);
+      assert!(tm.get(&Point{x:2, y:3}) == 17);
+    }
+
+    #[test]
+    fn test_rotate_cw() {
+      let mut tm = TerrainMap::<usize>::new(Dims{width: 2, height: 2, ..Default::default()});
+      tm.setc(0,0,1);
+
+      assert_eq!(tm.getc(0,0), 1);
+
+      tm.rotate_cw();
+      assert_eq!(tm.getc(0,0), 0);
+      assert_eq!(tm.getc(1,0), 1);
+
+      tm.rotate_cw();
+      assert_eq!(tm.getc(1,0), 0);
+      assert_eq!(tm.getc(1,1), 1);
+
+      tm.rotate_cw();
+      assert_eq!(tm.getc(1,1), 0);
+      assert_eq!(tm.getc(0,1), 1);
+
+      tm.rotate_cw();
+      assert_eq!(tm.getc(0,1), 0);
+      assert_eq!(tm.getc(0,0), 1);
+
+   }
+    #[test]
+    fn test_rotate_acw() {
+      let mut tm = TerrainMap::<usize>::new(Dims{width: 3, height: 3, ..Default::default()});
+      tm.setc(0,0,1);
+
+      assert_eq!(tm.getc(0,0), 1);
+
+      tm.rotate_acw();
+      assert_eq!(tm.getc(0,0), 0);
+      assert_eq!(tm.getc(0,2), 1);
+
+      tm.rotate_acw();
+      assert_eq!(tm.getc(0,2), 0);
+      assert_eq!(tm.getc(2,2), 1);
+
+      tm.rotate_acw();
+      assert_eq!(tm.getc(2,2), 0);
+      assert_eq!(tm.getc(2,0), 1);
+
+      tm.rotate_acw();
+      assert_eq!(tm.getc(2,0), 0);
+      assert_eq!(tm.getc(0,0), 1);
     }
 }
  
